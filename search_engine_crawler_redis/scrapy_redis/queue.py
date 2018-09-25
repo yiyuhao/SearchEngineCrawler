484adde64879f6975d174f0c87087a323586fcb7
yiyuhao <yiyuhao@mixadx.com>
2018-09-25 18:50:59 +0800

    fix: settings

--- search_engine_crawler_redis/scrapy_redis/queue.py
+++ search_engine_crawler_redis/scrapy_redis/queue.py
@@ -137,7 +137,6 @@ class PriorityQueue(Base):
         # kwargs only accepts strings, not bytes.
         self.server.execute_command('ZADD', self.key, score, data)
         self._record_request_push(request.meta['search_request_id'])  # record then fetch one by one from different requests
-        print(f'push {request.meta["search_request_id"]}      now=========={self.requests}')
 
     def pop(self, timeout=0):
         """
@@ -152,7 +151,7 @@ class PriorityQueue(Base):
             for redis_request in all_requests:
                 id_ = self.serializer.loads(redis_request)['meta']['search_request_id']
                 if id_ == next_id:
-                    print(f'pop ====={id_}===== list: {self.requests}, cur_id: {self.cur_request_id}')
+                    # print(f'pop ====={id_}===== list: {self.requests}, cur_id: {self.cur_request_id}')
                     self.cur_request_id = id_
                     self._record_request_pop(id_)
                     self.server.zrem(self.key, redis_request)
@@ -165,7 +164,7 @@ class PriorityQueue(Base):
         results, count = pipe.execute()
         if results:
             id_ = self.serializer.loads(results[0])['meta']['search_request_id']
-            print(f'pop ====={id_}===== list: {self.requests}, cur_id: {self.cur_request_id}')
+            # print(f'pop ====={id_}===== list: {self.requests}, cur_id: {self.cur_request_id}')
             self.cur_request_id = id_
             self._record_request_pop(id_)
             return self._decode_request(results[0])
--- search_engine_crawler_redis/search_engine_crawler_redis/controller/task_scheduler.py
+++ search_engine_crawler_redis/search_engine_crawler_redis/controller/task_scheduler.py
@@ -8,7 +8,52 @@ class Scheduler:
 
     def __init__(self, spider):
         self.spider = spider
-        self.scrapy_requests = []
+        self.scrapy_requests = {}
+        self.cur_request_id = None
+
+    @property
+    def next_request_id(self):
+        if not self.scrapy_requests:
+            return None
+
+        if self.cur_request_id is None:
+            result = next(iter(self.scrapy_requests.keys()))  # first elem of self.requests
+        else:
+            found = False
+            for id_ in self.scrapy_requests.keys():
+                if found is True:
+                    result = id_
+                    break
+                if self.cur_request_id == id_:
+                    found = True
+            else:
+                result = next(iter(self.scrapy_requests.keys()))
+
+        self.cur_request_id = result
+
+        return result
+
+    def _append_request(self, request):
+        id_ = request.meta['search_request_id']
+        request_list = self.scrapy_requests.get(id_)
+        if not request_list:
+            request_list = []
+            self.scrapy_requests[id_] = request_list
+
+        request_list.append(request)
+
+    def _fetch_one(self):
+        if not self.scrapy_requests:
+            return None
+
+        next_id = self.next_request_id
+
+        request = self.scrapy_requests[next_id].pop()
+
+        if not self.scrapy_requests[next_id]:
+            del self.scrapy_requests[next_id]
+
+        return request
 
     def fetch_one_request(self):
         db_manager = SearchRequestDBManager()
@@ -18,8 +63,6 @@ class Scheduler:
             for search_request in db_query_result:
                 rule = Rule(self.spider, *search_request)
                 for req in rule.scrapy_requests:
-                    self.scrapy_requests.append(req)
+                    self._append_request(req)
 
-            random.shuffle(self.scrapy_requests)
-
-        return self.scrapy_requests.pop() if self.scrapy_requests else None
+        return self._fetch_one()
